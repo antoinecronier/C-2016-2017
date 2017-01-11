9085dc5dc6f7e147a055a1dcff8921d2a15f45d0
JeromePavic <jeromepavic@gmail.com>
2017-01-11 16:17:06 +0100

    add StructureUserControl files

--- wpfzoo/views/usercontrols/StructureUserControl.xaml.cs
+++ wpfzoo/views/usercontrols/StructureUserControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wpfzoo.entities;
 
 namespace wpfzoo.views.usercontrols
 {
@@ -23,6 +24,29 @@ namespace wpfzoo.views.usercontrols
         public StructureUserControl()
         {
             InitializeComponent();
+
+            Structure structure = new entities.Structure();
+
+            structure.Name = "cage";
+            structure.Surface = 1000;
+            structure.AssignAnimals = new List<Animal>();
+            structure.AssignEmployees = new List<Employee>();
+            structure.Schedule = new Schedule();
+
+            this.label0.Content = "Name";
+            this.textBox0.Text = structure.Name;
+
+            this.label1.Content = "Surface";
+            this.textBox1.Text = structure.Surface.ToString();
+
+            this.label2.Content = "AssignAnimals";
+            this.textBox2.Text = structure.AssignAnimals.ToString();
+
+            this.label3.Content = "AssignEmployees";
+            this.textBox3.Text = structure.AssignEmployees.ToString();
+
+            this.label4.Content = "Schedule";
+            this.textBox4.Text = structure.Schedule.ToString();
         }
     }
 }
