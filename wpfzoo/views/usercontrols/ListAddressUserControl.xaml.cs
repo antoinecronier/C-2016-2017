888b671d17e529bfd284cc048da8787db8830bdd
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-01-11 16:01:28 +0100

    add list UC for address animal employee

---
+++ wpfzoo/views/usercontrols/ListAddressUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfzoo.views.usercontrols
+{
+    /// <summary>
+    /// Logique d'interaction pour ListAddressUserControl.xaml
+    /// </summary>
+    public partial class ListAddressUserControl : UserControl
+    {
+        public ListAddressUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
