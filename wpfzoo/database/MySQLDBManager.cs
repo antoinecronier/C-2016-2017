526cf87f5f63d07451dbb3301e8cb463363dd58d
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-01-12 13:42:46 +0100

    start db

---
+++ wpfzoo/database/MySQLDBManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpfzoo.entities;
+
+namespace wpfzoo.database
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class MySQLDBManager : DbContext
+    {
+        public DbSet<Animal> animalTable;
+
+        public void insert(Animal animal)
+        {
+            this.animalTable.Add(animal);
+            this.SaveChanges();
+        }
+    }
+}
