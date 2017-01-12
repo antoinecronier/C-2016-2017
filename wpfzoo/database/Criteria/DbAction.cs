ec1b01be5e2e85ce0232533733df80533701da48
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-01-12 16:28:10 +0100

    update DB management and item generation

---
+++ wpfzoo/database/Criteria/DbAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfzoo.database.criteria
+{
+    public enum DbAction
+    {
+        [StringValue("SELECT")]
+        SELECT,
+        [StringValue("DELETE")]
+        DELETE,
+        [StringValue("UPDATE")]
+        UPDATE
+    }
+}
---
+++ wpfzoo/database/Criteria/DbLinks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfzoo.database.criteria
+{
+    public enum DbLinks
+    {
+        [StringValue("INNER JOIN")]
+        INNERJOIN,
+        [StringValue("FROM")]
+        FROM
+    }
+}
