b2e968fef8561e79fd60b951ab06923db57d9970
NonlyNerd <notonlyanerd@gmail.com>
2017-02-05 15:14:23 +0100

    Set Planning UC to take Dictionnary structures and load them

--- wpfzoo/views/usercontrols/PlanningUserControl.xaml.cs
+++ wpfzoo/views/usercontrols/PlanningUserControl.xaml.cs
@@ -26,56 +26,41 @@ namespace wpfzoo.views.usercontrols
         #endregion
 
         #region properties
-        public ListView KeysList { get; set; }
-        public ListView ValuesList { get; set; }
-        public ObservableCollection<Schedule> Key { get; set; }
-        public ObservableCollection<Structure> Value { get; set; }
+        public ListView PlanningList { get; set; }
         #endregion
 
         #region constructor
         public PlanningUserControl()
         {
             this.InitializeComponent();
-            Key = new ObservableCollection<Schedule>();
-            Value = new ObservableCollection<Structure>();
-            this.keyList.ItemsSource = Key;
-            this.valueList.ItemsSource = Value;
-            this.KeysList = this.keyList;
-            this.ValuesList = this.valueList;
 
             //testcode
-            Dictionary<Schedule, Structure> planning = new Dictionary<Schedule, Structure>();
-            Schedule horaire = new Schedule();
-            horaire.Start = DateTime.Now;
-            horaire.End = DateTime.Now.AddHours(2);
-            Schedule nettoyage = new Schedule();
-            nettoyage.Start = DateTime.Now.AddHours(2);
-            nettoyage.End = DateTime.Now.AddHours(0.2);
-            Structure cage = new Structure();
-            cage.Name = "Cajolions";
-            Structure toilettes = new Structure();
-            toilettes.Name = "Popo";
+            //Dictionary<Schedule, Structure> planning = new Dictionary<Schedule, Structure>();
+            //Schedule horaire = new Schedule();
+            //horaire.Start = DateTime.Now;
+            //horaire.End = DateTime.Now.AddHours(2);
+            //Schedule nettoyage = new Schedule();
+            //nettoyage.Start = DateTime.Now.AddHours(2);
+            //nettoyage.End = nettoyage.Start.AddHours(0.2);
+            //Structure cage = new Structure();
+            //cage.Name = "Cajolions";
+            //Structure toilettes = new Structure();
+            //toilettes.Name = "Popo";
 
-            planning.Add(horaire, cage);
-            planning.Add(nettoyage, toilettes);
+            //planning.Add(horaire, cage);
+            //planning.Add(nettoyage, toilettes);
 
-            this.LoadItem(planning);
+            //this.LoadItem(planning);
         }
         #endregion
 
         #region methods
         /// <summary>
-        /// Current list for User items.
+        /// Current list for Planning items.
         /// </summary>
         public void LoadItem(Dictionary<Schedule, Structure> items)
         {
-            Key.Clear();
-            Value.Clear();
-            foreach (var item in items)
-            {
-                Key.Add(item.Key);
-                Value.Add(item.Value);
-            }
+            Resources["Plannings"] = items;
         }
         #endregion
 
