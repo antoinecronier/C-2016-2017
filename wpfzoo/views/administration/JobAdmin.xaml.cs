b3fb46e3fc234c7be868b98cf5e82b23274fd22b
Xely <julien_guya@msn.com>
2017-02-08 16:19:53 +0100

    added JobAdminVM

--- wpfzoo/views/administration/JobAdmin.xaml.cs
+++ wpfzoo/views/administration/JobAdmin.xaml.cs
@@ -17,76 +17,16 @@ using wpfzoo.entities.bases;
 using System.ComponentModel;
 using wpfzoo.database;
 using System.Collections.ObjectModel;
+using wpfzoo.viewmodel;
 
 namespace wpfzoo.views.administration
 {
-    /// <summary>
-    /// Interaction logic for JobAdmin.xaml
-    /// </summary>
     public partial class JobAdmin : Page
     {
-        ObservableCollection<Job> jobList = new ObservableCollection<Job>();
-        ObservableCollection<Schedule> scheduleList = new ObservableCollection<Schedule>();
         public JobAdmin()
         {
             InitializeComponent();
-            new MySQLFullDB();
-            this.UCJobList.itemList.SelectionChanged += ItemList_SelectionChanged;
-            InitLists();
+            this.DataContext = new JobAdminVM(this);
         }
-
-        private void ItemList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.AddedItems.Count > 0)
-            {
-                Job job = (e.AddedItems[0] as Job);
-            }
-        }
-        private async void InitLists()
-        {
-            MySQLManager<Job> jobManager = new MySQLManager<Job>();
-            this.UCJobList.LoadItem((await jobManager.Get()).ToList());
-
-            //MySQLManager<Schedule> scheduleManager = new MySQLManager<Schedule>();
-            //this.UCScheduleList.LoadItem((await scheduleManager.Get()).ToList());
-        }
-
-        
-
-        //
-        //private Job job;
-        //private String scheduleToString;
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //public Job Job
-        //{
-        //    get { return job; }
-        //    set { job = value; }
-        //}
-
-        //public String ScheduleToString
-        //{
-        //    get
-        //    {
-        //        return job.Schedule.Start.ToString() + job.Schedule.End.ToString();
-        //    }
-
-        //    set
-        //    {
-        //        scheduleToString = value;
-        //        //OnPropertyChanged("ScheduleToString");
-        //    }
-        //}
-
-        //public JobAdmin()
-        //{
-        //    InitializeComponent();
-        //    MySQLFullDB fullDb = new MySQLFullDB();
-        //    //Schedule schedule = new Schedule();
-        //    //schedule.Start = DateTime.Now;
-        //    //schedule.End = DateTime.Now;
-        //    //this.scheduleUC.Schedule = schedule;
-        //}
     }
 }
