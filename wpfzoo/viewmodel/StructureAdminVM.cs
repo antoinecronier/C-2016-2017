705ae70afcf7b1cbc97ca4157ac11f188fc9be01
Edrenn <edern.rodriguez@orange.fr>
2017-02-08 12:11:52 +0100

    Ajout StructureVM

--- wpfzoo/viewmodel/StructureAdminVM.cs
+++ wpfzoo/viewmodel/StructureAdminVM.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wpfzoo.database;
+using wpfzoo.entities;
+using wpfzoo.views.administration;
 
 namespace wpfzoo.viewmodel
 {
-    class StructureAdminVM
+    public class StructureAdminVM
     {
+        private Structure currentStructure;
+        private StructureAdmin structureAdmin;
+        private MySQLManager<Structure> structureManager = new MySQLManager<Structure>();
+
+
+        public StructureAdminVM(StructureAdmin structureAdmin)
+        {
+            this.structureAdmin = structureAdmin;
+
+            InitUC();
+            InitActions();
+        }
+
+        private void InitUC()
+        {
+            currentStructure = new Structure();
+            this.structureAdmin.ucStructure.Structure = currentStructure;
+        }
+
+        private void InitActions()
+        {
+            this.structureAdmin.btnValidate.Click += BtnValidate_Click;
+        }
+
+        private void BtnValidate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            structureManager.Insert(this.structureAdmin.ucStructure.Structure);
+        }
+
+        private void Update()
+        {
+            this.structureAdmin.Ok.Click += BtnUpdate_Click;
+        }
+
+        private void BtnUpdate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            structureManager.Update(this.structureAdmin.ucStructure.Structure);
+        }
+
+        private void Delete()
+        {
+            this.structureAdmin.Delete.Click += BtnDelete_Click;
+        }
+
+        private void BtnDelete_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            structureManager.Delete(this.structureAdmin.ucStructure.Structure);
+        }
     }
 }
