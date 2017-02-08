83b94fccad225079357fbfe1b015bfe5ab35b310
NonlyNerd <notonlyanerd@gmail.com>
2017-02-08 14:08:46 +0100

    Adding lists to AddressAdminVM

--- wpfzoo/viewmodel/AddressAdminVM.cs
+++ wpfzoo/viewmodel/AddressAdminVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using wpfzoo.database;
 using wpfzoo.entities;
 using wpfzoo.views.administration;
@@ -18,24 +19,41 @@ namespace wpfzoo.viewmodel
         public AddressAdminVM(AddressAdmin addressAdmin)
         {
             this.addressAdmin = addressAdmin;
+            InitLists();
             InitUC();
             InitActions();
+        }
+
+        private async void InitLists()
+        {
+            this.addressAdmin.UCAddressList.LoadItem((await addressManager.Get()).ToList());
         }
 
         private void InitUC()
         {
             currentAddress = new Address();
-            this.addressAdmin.ucAddress.Address = currentAddress;
+            this.addressAdmin.UCAddress.Address = currentAddress;
+
         }
 
         private void InitActions()
         {
             this.addressAdmin.btnValidate.Click += BtnValidate_Click;
+            this.addressAdmin.UCAddressList.ItemsList.SelectionChanged += ItemsList_SelectionChanged;
+        }
+
+        private void ItemsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                Address item = (e.AddedItems[0] as Address);
+                this.addressAdmin.UCAddress.Address = item;
+            }
         }
 
         private void BtnValidate_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            addressManager.Insert(this.addressAdmin.ucAddress.Address);
+            addressManager.Insert(this.addressAdmin.UCAddress.Address);
         }
     }
 }
