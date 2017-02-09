c1c540eab205c6f9b5559339413ab43af1214462
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-02-09 17:02:35 +0100

    patch attach

--- wpfzoo/database/entitieslinks/MySQLJobManager.cs
+++ wpfzoo/database/entitieslinks/MySQLJobManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace wpfzoo.database.entitieslinks
     {
         public void GetSchedule(Job job)
         {
-            this.DbSetT.Attach(job);
+            bool isDetached = this.Entry(job).State == EntityState.Detached;
+            if (isDetached)
+                this.DbSetT.Attach(job);
             this.Entry(job).Reference(x => x.Schedule).Load();
         }
     }
